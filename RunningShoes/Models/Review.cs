f88868e6eb8815c2c00d414040256880ea60bd1c
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2024-02-29 09:08:45 +0200

    shoes and review models made

---
+++ RunningShoes/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace RunningShoes.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Reviewer { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Rating { get; set; }
+        public DateTime ReviewDate { get; set; }
+    }
+}
--- RunningShoes/Models/RunningShoes.cs
+++ RunningShoes/Models/RunningShoes.cs
@@ -2,5 +2,12 @@
 {
     public class RunningShoes
     {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Name { get; set; }    
+        public string Type { get; set; }
+        public double Weight { get; set; }  
+        public double Price { get; set; }
+        public DateTime ReleaseYear { get; set; }
     }
 }
