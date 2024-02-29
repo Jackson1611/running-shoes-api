1da24af69c98313bbefe08222b7e769778a459d3
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2024-02-29 17:51:01 +0200

    added db-context

---
+++ RunningShoes/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunningShoes.Models;
+
+namespace RunningShoes.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Shoe> Shoes { get; set; }
+
+       
+
+            
+        
+    }
+}
--- RunningShoes/Models/Shoe.cs
+++ RunningShoes/Models/Shoe.cs
@@ -1,13 +1,14 @@
 ﻿namespace RunningShoes.Models
 {
-    public class RunningShoes
+    public class Shoe
     {
         public int Id { get; set; }
         public string Brand { get; set; }
-        public string Name { get; set; }    
+        public string Name { get; set; }
         public string Type { get; set; }
-        public double Weight { get; set; }  
+        public double Weight { get; set; }
         public double Price { get; set; }
         public DateTime ReleaseYear { get; set; }
+
     }
 }
