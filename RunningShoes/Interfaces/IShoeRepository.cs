64ec09f73d8166710feddc1e1a439ab2e9dbeac8
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2024-03-03 10:24:08 +0200

    started implementing repository patern and added interfaces

---
+++ RunningShoes/Interfaces/IShoeRepository.cs
@@ -0,0 +1,15 @@
+﻿using RunningShoes.Models;
+
+namespace RunningShoes.Interfaces
+{
+    public interface IShoeRepository
+    {
+        Task<IEnumerable<Shoe>> GetAllAsync();
+        Task<Shoe> GetByIdAsync(int id);
+        Task<IEnumerable<Shoe>> SearchAsync(string query);
+        Task AddAsync(Shoe shoe);
+        Task UpdateAsync(Shoe shoe);
+        Task DeleteAsync(int id);
+
+    }
+}
