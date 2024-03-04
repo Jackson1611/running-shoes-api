39712d1e498ba6493fb33893a5984c7ed740ef7c
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2024-03-04 10:38:45 +0200

    implement repository pattern

---
+++ RunningShoes/Repository/ShoeRepository.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RunningShoes.Interfaces;
+using RunningShoes.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace RunningShoes.Repository
+
+{
+    public class ShoeRepository: IShoeRepository
+    {
+        private readonly RunningShoesDbContext _context;
+
+        public ShoeRepository(RunningShoesDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Shoe>> GetAllAsync()
+        {
+            return await _context.Shoes.ToListAsync();
+        }
+
+        public async Task<Shoe> GetByIdAsync(int id)
+        {
+            return await _context.Shoes.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Shoe>> SearchAsync(string query)
+        {
+            return await _context.Shoes
+                .Where(s => s.Name.Contains(query) || s.Brand.Contains(query))
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Shoe shoe)
+        {
+            _context.Shoes.Add(shoe);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Shoe shoe)
+        {
+            _context.Entry(shoe).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var shoe = await _context.Shoes.FindAsync(id);
+            _context.Shoes.Remove(shoe);
+            await _context.SaveChangesAsync();
+        }
+    }   
+    {
+    }
+}
